8743faf012b730aef84c92c7f7e1c63ed03f817d
AdriCQ <adrian.capote95@zoho.com>
2022-12-27 14:43:01 -0500

    artisan test:seed command

---
+++ app/Console/Commands/TestSeed.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class TestSeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:seed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Seed databases for testing';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('migrate:fresh --seed --seeder FakeSeeder');
+        Artisan::call('module:seed --class FakeSeeder');
+        $this->info('Database Seeding Done!!!');
+    }
+}
