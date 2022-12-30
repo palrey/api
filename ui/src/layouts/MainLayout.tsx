67f45de904451c4b374514d50437448e09d6ebc0
AdriCQ <adrian.capote95@zoho.com>
2022-12-30 18:31:12 -0500

    ui main layout md view

--- ui/src/layouts/MainLayout.tsx
+++ ui/src/layouts/MainLayout.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import LeftDrawer from '@/components/drawers/LeftDrawer';
+import {
+    DrawerItem,
+    ItemsContainer,
+    LeftDrawer,
+    ItemDivisor
+} from '@/components/drawers';
 import Navbar from '@/components/navs/Navbar';
 import { Outlet } from 'react-router-dom';
+import { mdiViewDashboard } from '@mdi/js';
 
 function MainLayout() {
     return (
-        <LeftDrawer>
-            <Navbar />
-            <Outlet />
+        <LeftDrawer
+            items={
+                <ItemsContainer>
+                    <DrawerItem label="Hello" header className="mt-2" />
+                    <DrawerItem label="Hello" icon={mdiViewDashboard} />
+                    <DrawerItem label="Hello" icon={mdiViewDashboard} />
+                    <ItemDivisor />
+                    <DrawerItem label="Hello" header />
+                    <DrawerItem label="Hello" icon={mdiViewDashboard} />
+                </ItemsContainer>
+            }
+        >
+            <Navbar title="Administracion" />
+            <div className="mt-16 sm:mt-0">
+                <Outlet />
+            </div>
         </LeftDrawer>
     );
 }
