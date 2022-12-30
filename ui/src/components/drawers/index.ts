c37b3735a4a953003d6a5fdcbf375ea203cd2c53
AdriCQ <adrian.capote95@zoho.com>
2022-12-30 17:50:44 -0500

    ui drawer divisor

--- ui/src/components/drawers/index.ts
+++ ui/src/components/drawers/index.ts
@@ -1,5 +1,6 @@
 import DrawerItem from './DrawerItem';
 import LeftDrawer from './LeftDrawer';
 import ItemsContainer from './ItemsContainer';
+import ItemDivisor from './ItemDivisor';
 
-export { DrawerItem, LeftDrawer, ItemsContainer }
+export { DrawerItem, LeftDrawer, ItemsContainer, ItemDivisor }
